d5fa2fbcfb76b53929a15b6863472ea061923ae8
Michael Eselgroth <meselgroth@yahoo.com>
2014-09-08 15:29:30 +0100

    Update nuget packages. Publish to azure.

---
+++ AzureAccess/Queue.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureAccess
+{
+    public class Queue : IQueue
+    {
+        public Hashtag Dequeue()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
